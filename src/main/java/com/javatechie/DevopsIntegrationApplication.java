a6a0e8485878b07da06052486565b47e8d5015ad
suraj <surajsah544@gmail.com>
2024-10-23 21:50:24 +0530

    Initial commit after cloning

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to suraj world";
 	}
 
 	public static void main(String[] args) {
